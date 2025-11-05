0e0a4a56c4d1f2a130e5f10d87878f4871f02782
theressathebez <153163348+theressathebez@users.noreply.github.com>
2025-11-05 09:53:28 +0700

    COBA

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback </title>
+    <title>Feedback LALALALALA</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
